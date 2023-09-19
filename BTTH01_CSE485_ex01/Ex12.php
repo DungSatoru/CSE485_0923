00f7b0ef3163afc490f0ca527a853465e9784b2c
DungSatoru <Dungsatoru.dev@gmail.com>
2023-09-19 13:44:19 +0700

    Done TH1a

--- BTTH01_CSE485_ex01/Ex12.php
+++ BTTH01_CSE485_ex01/Ex12.php
@@ -1,14 +1,35 @@
 <?php
-$numbers = [
+$numbers = array(
     'key1' => 12,
     'key2' => 30,
     'key3' => 4,
     'key4' => -123,
     'key5' => 1234,
     'key6' => -12565,
-];
+);
 
-echo "</br>❖ Phần tử đầu tiên (" . reset($numbers) . "), phần tử cuối cùng (" . end($numbers) . ")";
-echo "</br>❖ Số lớn nhất (" . max($numbers) . "), số nhỏ nhất (" . min($numbers) . "), tổng các giá trị từ mảng trên (" . array_sum($numbers) . ")";
-echo "</br>❖ Sắp xếp mảng theo chiều tăng, giảm các giá trị";
-echo "</br>❖ Sắp xếp mảng theo chiều tăng, giảm các key";
+echo 'Phần tử đầu tiên của mảng' . reset($numbers) . '<br>';
+echo 'Phần tử cuối cùng của mảng: ' . end($numbers) . '<br>';
+echo 'Phần tử lon nhất của mảng: ' . max($numbers) . '<br>';
+echo 'Phần tử nho nhất của mảng: ' . min($numbers) . '<br>';
+echo 'Tổng các giá trị của mảng: ' . array_sum($numbers) . '<br>';
+asort($numbers);
+echo '<br>Sắp xếp tăng (value): ';
+foreach ($numbers as $s_value) {
+    echo $s_value . '; ';
+}
+ksort($numbers);
+echo '<br>Sắp xếp tăng (key)';
+foreach ($numbers as $s) {
+    echo $s . '; ';
+}
+arsort($numbers);
+echo '<br>Sắp xếp giảm (value): ';
+foreach ($numbers as $s_value) {
+    echo $s_value . '; ';
+}
+krsort($numbers);
+echo '<br>Sắp xếp giảm (key)';
+foreach ($numbers as $s) {
+    echo $s . '; ';
+}
